bf282717a881c26ae289752a07642dd3b497a48e
Daniel Camargo <drubiocamargo@gmail.com>
2021-10-06 19:13:13 -0300

    Mudanças no trabalho de Técnicas!

--- Técnicas de Programação/Projetos/Menu Alunos/funcoes.py
+++ Técnicas de Programação/Projetos/Menu Alunos/funcoes.py
@@ -1,9 +1,14 @@
 # Modulo para as funções
 
-def criar_cabecalho(titulo:str,tamanhoLinha = 55):
-    print("-"*tamanhoLinha)
-    print(titulo.center(tamanhoLinha))
-    print("-"*tamanhoLinha)
+def criar_cabecalho(titulo:str,hasEqual = False,tamanhoLinha = 55):
+    if hasEqual == True:
+        print("-=-"*18)
+        print(titulo.center(tamanhoLinha))
+        print("-=-"*18)
+    else:
+        print("-"*tamanhoLinha)
+        print(titulo.center(tamanhoLinha))
+        print("-"*tamanhoLinha)
 
 
 def criar_menu(lista,titulo:str,tamanhoLinha = 55):
@@ -11,7 +16,7 @@ def criar_menu(lista,titulo:str,tamanhoLinha = 55):
     print(titulo.center(tamanhoLinha))
     print("-"*tamanhoLinha)
     for i,c in enumerate(lista):
-        print("{} --> {}".format(i+1,c))
+        print("[{}] {}".format(i+1,c))
     print("-"*tamanhoLinha)
 
 
@@ -35,22 +40,35 @@ def exibir_usuarios_cadastrados_ordem_alfabetica(lista):
         print("{} --> {}".format(i+1,c))
 
 
-def verificar_usuario(lista,nome:str):
+def verificar_usuario_nome(lista,nome:str):
     for i,c in enumerate(lista):
         if c["nome"] == nome:
-            print("Há um aluno com esse nome!")
+            return True
             break
         elif i == len(lista) - 1:
-            print("Não encontramos nenhum aluno com esse nome!")
+            return False
+
+
+def verificar_usuario_email(lista,email:str):
+    for i,c in enumerate(lista):
+        if c["email"] == email:
+            return True
+            break
+        elif i == len(lista) - 1:
+            return False
 
 
 def remover_aluno(lista,email:str):
     for i,c in enumerate(lista):
         if c["email"] == email:
             lista.pop(i)
+            print("Aluno {} removido da lista com sucesso!".format(c["nome"]))
 
 
 def alterar_nome_usuario(lista,email:str,novoNome:str):
+    cont = 0
     for i,c in enumerate(lista):
         if c["email"] == email:
+            print("Alteração de {} para {} foi um sucesso!".format(c["nome"],novoNome))
             c["nome"] = novoNome
+            break
