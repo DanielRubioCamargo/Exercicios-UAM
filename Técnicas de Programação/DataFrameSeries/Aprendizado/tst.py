3582904338b728c3066ca1eedec7d3ad5d04985f
Daniel Camargo <drubiocamargo@gmail.com>
2021-11-21 17:04:37 -0300

    Sys!

---
+++ Técnicas de Programação/DataFrameSeries/Aprendizado/tst.py
@@ -0,0 +1,10 @@
+import sys
+
+#print(sys.platform)
+#print(sys.copyright)
+#print(sys.version)
+sys.stdout.write("Hello\n")
+sys.stderr.write("jdfklsa\n")
+a = "Hello world hahaha"
+print(sys.getsizeof(a))
+sys.setrecursionlimit(10000)
