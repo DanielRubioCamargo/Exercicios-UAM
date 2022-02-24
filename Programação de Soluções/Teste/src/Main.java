2a915531c429767db0bf636a6321b210332a2ce4
Daniel Camargo <drubiocamargo@gmail.com>
2022-02-24 18:02:52 -0300

    Teste Final!

--- Programação de Soluções/Teste/src/Main.java
+++ Programação de Soluções/Teste/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(1);
+        System.out.println("Teste final");
     }
 
 }
